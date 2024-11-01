50f2599f3157a92c9996f6bf74585f2a778781d7
The Sledgehammer <163490470+tsushima12@users.noreply.github.com>
2024-11-01 19:49:51 +0530

    Create Therealcode.py

---
+++ Therealcode.py
@@ -0,0 +1,7 @@
+while True:
+    n = input("Type 'Hello World': ")
+    if n == "Hello World":
+        print("Nice! You got it right.")
+        break
+    else:
+        print("Hell naah, try again and type exactly 'Hello World'")
